3d0ca67c469f60b25a5de637b5b22b6b52e30a94
joey.r.h <joey.ren.huang@gmail.com>
2021-10-29 20:27:13 +0800

    1.0.5

--- index.js
+++ index.js
@@ -25,13 +25,12 @@ ReactDisplayNameWebpackPlugin.prototype.apply = function(c) {
 ReactDisplayNameWebpackPlugin.prototype.working = function (c) {
   c.hooks.compilation.tap('ReactDisplayNameWebpackPlugin', (compilation, compilationParams) => {
     compilation.hooks.succeedModule.tap('ReactDisplayNameWebpackPlugin', (m) => {
-      const {type, rawRequest, _source, request, userRequest} = m
-      if (type === 'javascript/auto' && (userRequest || request) && (userRequest || request).match(/jsx?$/) && (userRequest || request).indexOf('node_modules') === -1 
+      const {type, rawRequest, _source, userRequest} = m
+      if (type === 'javascript/auto' && userRequest && userRequest.match(/jsx?$/) && userRequest.indexOf('node_modules') === -1 
         && (_source?._valueAsString || _source?._value)) {
-        userRequest = userRequest || request
         let id = this.s && userRequest.indexOf(this.s) !== -1 && userRequest.substr(userRequest.indexOf(this.s))
         !id && this.n && (id = userRequest.split('/').slice(this.n * -1).join('/'))
-        !id && (id = rawRequest)
+        !id && (id = rawRequest || userRequest)
         let appendstr = displayName.replace(/{{id}}/mg, id).replace(/{{force}}/mg, this.force)
         _source._valueAsString && (_source._valueAsString += appendstr)
         _source._value && (_source._value += appendstr)
